5c7621e26734db06fbc7775a53cb9bcf997a7370
Arief Wijaya <ariefwiijaya@gmail.com>
2021-05-22 19:45:59 +0700

    added label to custom text form

--- lib/views/component/common_form.dart
+++ lib/views/component/common_form.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
 
 class CTextFormFilled extends StatelessWidget {
+  final String? labelText;
   final String? hintText;
   final void Function(String?)? onSaved;
   final String? Function(String?)? validator;
-  const CTextFormFilled({Key? key, this.hintText, this.onSaved, this.validator})
+  final int? maxLines;
+  const CTextFormFilled(
+      {Key? key, this.labelText, this.hintText,this.maxLines=1, this.onSaved, this.validator})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(hintText: hintText, border: OutlineInputBorder()),
-      onSaved: onSaved,
-      validator: validator,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (labelText != null) Text(labelText!),
+        TextFormField(
+          decoration:
+              InputDecoration(hintText: hintText, border: OutlineInputBorder()),
+          onSaved: onSaved,
+          validator: validator,
+          maxLines: maxLines,
+        ),
+      ],
     );
   }
 }
