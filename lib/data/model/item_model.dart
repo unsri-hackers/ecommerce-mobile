473d23eb28cdb4ca14239900e99cd771d5995e89
ichsanachmad <ichsanachmad33@gmail.com>
2021-05-23 18:20:11 +0700

    finish item model

---
+++ lib/data/model/item_model.dart
@@ -0,0 +1,36 @@
+class ItemModel {
+  late String id;
+  late String name;
+  late int price;
+  late String condition;
+  late double weight;
+  late String filename;
+
+  ItemModel(
+      {required this.id,
+      required this.name,
+      required this.price,
+      required this.condition,
+      required this.weight,
+      required this.filename});
+
+  ItemModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    price = json['price'];
+    condition = json['condition'];
+    weight = json['weight'];
+    filename = json['filename'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['price'] = this.price;
+    data['condition'] = this.condition;
+    data['weight'] = this.weight;
+    data['filename'] = this.filename;
+    return data;
+  }
+}
---
+++ lib/data/repository/item_repository.dart
@@ -0,0 +1,7 @@
+import 'package:deuvox/app/utils/network_utils.dart';
+
+class ItemRepository {
+  final NetworkUtils _networkUtils = NetworkUtils();
+
+  
+}
