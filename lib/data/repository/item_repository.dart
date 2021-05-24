088b6617cc910dc34667ac65f1dfd9d9019d5882
ichsanachmad <ichsanachmad33@gmail.com>
2021-05-24 09:09:43 +0700

    finish repository, finish domain

--- lib/data/repository/item_repository.dart
+++ lib/data/repository/item_repository.dart
@@ -1,12 +1,14 @@
 import 'package:deuvox/app/constant/status.dart';
 import 'package:deuvox/app/utils/network_utils.dart';
 import 'package:deuvox/data/model/base_response.dart';
+import 'package:deuvox/data/model/upload_item_model.dart';
 
 class ItemRepository {
   final NetworkUtils _networkUtils = NetworkUtils();
 
-  Future<BaseResponse<void>> uploadItem() async {
-    Map<String, dynamic> result = await _networkUtils.post('');
+  Future<BaseResponse<void>> uploadItem(UploadItemModel uploadItemModel) async {
+    Map<String, dynamic> result =
+        await _networkUtils.post('', body: uploadItemModel.toJson());
     BaseResponse<void> baseResponse =
         BaseResponse.fromJson(result, (json) => null);
     if (baseResponse.status == Status.SUCCESS)
