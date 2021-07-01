b645d87fc5b2e6fad5ace4750fa8ba280099992d
ichsanachmad <ichsanachmad33@gmail.com>
2021-07-01 20:07:31 +0700

    add profile api utils (mock)

--- lib/app/utils/api_utils.dart
+++ lib/app/utils/api_utils.dart
@@ -6,7 +6,7 @@ class ApiUtils {
   static String listProductsPaging =
       AppConfig.baseUrl + "/storefront/products/paging";
   static String addProduct = AppConfig.baseUrl + "/storefront/products";
-  static String userProfile = AppConfig.baseUrl + "/users";
+  static String userProfile = "https://run.mocky.io/v3/2beab3e1-b04c-4809-81bc-48d6f088ff19";
   //secret delete link:https://designer.mocky.io/manage/delete/2b89f6de-d30c-4b9c-ae89-ec36597483c4/deuvox
   static String onGoingOrders =
       "https://run.mocky.io/v3/22d13d9a-66b1-47d4-9f6c-3e21ed499189";
