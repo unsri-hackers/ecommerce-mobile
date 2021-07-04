81623b13b4bf885dbd74c0d449970f5430362c65
Arief Wijaya <ariefwiijaya@gmail.com>
2021-07-04 13:40:59 +0700

    change mockup url to dev deuvox

--- lib/app/utils/api_utils.dart
+++ lib/app/utils/api_utils.dart
@@ -13,6 +13,6 @@ class ApiUtils {
       "https://run.mocky.io/v3/22d13d9a-66b1-47d4-9f6c-3e21ed499189";
   static String productCategories =
       "https://run.mocky.io/v3/a0ea061d-b557-4f10-88aa-16232b4e7633";
-  static String yourProducts =
-      "https://run.mocky.io/v3/812e482c-0e66-441a-b597-6ed73642dc29";
+  static String yourProducts =addProduct;
+      // "https://run.mocky.io/v3/812e482c-0e66-441a-b597-6ed73642dc29";
 }
